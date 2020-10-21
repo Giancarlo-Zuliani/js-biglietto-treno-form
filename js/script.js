e736f0aaac06fea00d2545985b8c4f1dc72e35b9
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-21 16:47:12 +0200

    genera button added and scripted

--- js/script.js
+++ js/script.js
@@ -7,19 +7,26 @@ var userName = document.querySelector('input[name="name"]');
 var distance = document.querySelector('input[name="km"]');
 
 GENERA.addEventListener('click' , function(){
+  if (userName.value != ""  &&  distance.value != "" ){
+    let ticketCost = distance.value * PRICEPERKM;
+    let age = document.querySelector('input[name="eta"]:checked').value;
 
-  let ticketCost = distance.value * PRICEPERKM;
-  let age = document.querySelector('input[name="eta"]:checked').value;
+    if (age === "minorenne"){
+      ticketCost = (ticketCost / 100 ) * 80;
+    }else if (age === "over 65"){
+      ticketCost= (ticketCost / 100 ) * 60;
+    }
 
-  if (age === "minor"){
-    ticketCost = (ticketCost / 100 ) * 80;
-  }else if (age === "over"){
-    ticketCost= (ticketCost / 100 ) * 60;
+    let seatNum = Math.floor(Math.random() * 999);
+    let codeCp = 100000 + Math.floor(Math.random() * 900000 )
+
+    document.getElementById('ticketname').innerHTML = userName.value;
+    document.getElementById('discount').innerHTML = age;
+    document.getElementById('seat').innerHTML = seatNum;
+    document.getElementById('cpcode').innerHTML = codeCp;
+    document.getElementById('ticketcost').innerHTML = ticketCost.toFixed(2) + " Euro";
+    document.getElementById('ticketcontainer').style.display ="block";
+    userName.value ="";
+    distance.value="";
   }
-
-
-  console.log(userName.value);
-  console.log(distance.value)
-  console.log(ticketCost)
-
 })
