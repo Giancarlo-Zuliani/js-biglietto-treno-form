6e178f619b0891d657742ad3a7ab0ea54c0d70f9
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-21 17:35:52 +0200

    code clean and commented

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 
-const GENERA = document.querySelector('#genera');
+//BUTTONS
 
-const PRICEPERKM = 0.21;
+const GENERA = document.querySelector('#genera');
 
 const RESET = document.querySelector('#reset');
 
+//COST PER KM
+
+const PRICEPERKM = 0.21;
+
+//USER INPUT
 
 var userName = document.querySelector('input[name="name"]');
 var distance = document.querySelector('input[name="km"]');
 
+//TICKET GENERATOR FUNCTION
 
 GENERA.addEventListener('click' , function(){
   if (userName.value != ""  &&  distance.value != "" ){
@@ -20,21 +26,25 @@ GENERA.addEventListener('click' , function(){
     }else if (age === "over 65"){
       ticketCost= (ticketCost / 100 ) * 60;
     }
-
+    //SEAT NUMBER
     let seatNum = Math.floor(Math.random() * 999);
+    //CP CODE GENERATOR
     let codeCp = 100000 + Math.floor(Math.random() * 900000);
 
+    //WRITE DATA ON TICKET
     document.getElementById('ticketname').innerHTML = userName.value;
     document.getElementById('discount').innerHTML = age;
     document.getElementById('seat').innerHTML = seatNum;
     document.getElementById('cpcode').innerHTML = codeCp;
     document.getElementById('ticketcost').innerHTML = ticketCost.toFixed(2) + " Euro";
     document.getElementById('ticketcontainer').style.display ="block";
+    //RESET INPUT FIELD
     userName.value ="";
     distance.value="";
-  }
-})
+  };
+});
 
+//RESET BUTTON
 
 RESET.addEventListener('click',function(){
   document.getElementById('ticketcontainer').style.display ="none";
